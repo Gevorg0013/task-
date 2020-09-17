7307a53cd59b97d11dd587efa31334dd8829f036
Gevorg <48966291+Gevorg0013@users.noreply.github.com>
2020-09-17 18:06:36 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,32 +2,32 @@
 
 int main()
 {
-	const array_2D<abstract_cell_ptr> data = util::read("D:\\Helo.txt");
+    const array_2D<abstract_cell_ptr> data = util::read("./example_2.txt");
 
-	try
-	{
-		for (auto &i : data)
-		{
-			for (auto &j : i)
-			{
-				if (*j)
-				{
-					// If the cell is not empty
-					std::cout << (*j)(data) << ',';
-				}
-				else
-				{
-					// If the cell is empty
-					std::cout << ',';
-				}
-			}
-			std::cout << std::endl;
-		}
-	}
-	catch (const char *const str)
-	{
-		std::cerr << "Error during data calculation: " << str << std::endl;
-	}
-
-	return 0;
+    try
+    {
+        for (auto &i : data)
+        {
+            for (auto &j : i)
+            {
+                if (*j)
+                {
+                    // If the cell is not empty
+                    std::cout << (*j)(data) << ',';
+                }
+                else
+                {
+                    // If the cell is empty
+                    std::cout << ',';
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
+    catch (const char *const str)
+    {
+        std::cerr << "Error during data calculation: " << str << std::endl;
+    }
+    
+    return 0;
 }
